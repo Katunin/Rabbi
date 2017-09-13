a8af9516028a7310c9dfc99911bc16eb0cfbaf60
Vitalii Katunin <vitaliikatunin@gmail.com>
2017-09-14 00:02:49 +0300

    сделал адаптив, добавил кнопку поиска

--- src/js/init.js
+++ src/js/init.js
@@ -1,5 +1,22 @@
 $( function() {
 
+    // slick nav init
+    $('#menu').slicknav({
+        appendTo: '.header .flex-container.justify-sp-between',
+        label: ''
+    });
+
+    //search
+    $('a[href="#search"]').on('click', function(event) {
+        $('#search').addClass('open');
+        $('#search > form > input[type="search"]').focus();
+    });
+    $('#search, #search button.close').on('click keyup', function(event) {
+        if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
+            $(this).removeClass('open');
+        }
+    });
+
 
 });
 
